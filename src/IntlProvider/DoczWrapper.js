936d18867d6c25f0a129a21a0dd53dd96ca6b75c
Marcio Barrios <marcio.barrios@xing.com>
2019-05-24 01:13:18 +0200

    refactor: remove Docz wrapper

--- src/IntlProvider/DoczWrapper.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { IntlProvider } from "./IntlProvider"
-
-const Wrapper = ({ children, config }) => (
-  <IntlProvider config={config}>{children}</IntlProvider>
-)
-
-export default Wrapper
