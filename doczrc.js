57f164c1a562ee093001ac90cf6388ffbf6a3b99
Marcio Barrios <marcio.barrios@xing.com>
2019-05-23 12:29:30 +0200

    docs: update Docz documentation

--- doczrc.js
+++ doczrc.js
@@ -1,11 +1,4 @@
 export default {
   codeSandbox: false,
-  ignore: [
-    "changelog.md",
-    "code_of_conduct.md",
-    "contributing.md",
-    "license.md",
-  ],
-  // wrapper: "src/IntlProvider/DoczWrapper",
-  menu: ["Readme", "Components"],
+  menu: ["Introduction", "Recipes", "Components"],
 }
